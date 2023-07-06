a0942ee26868e87ed45a901d4702439261e64533
Brian Hartnett <brian.phartnettjr@gmail.com>
2023-07-05 23:46:16 -0400

    Added message, sum, and cards display

--- index.js
+++ index.js
@@ -4,17 +4,24 @@ let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
 
 function startGame() {
+    sumEl.textContent = "Sum: " + sum
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
     if (sum <= 20) {
-        message = "Hit or Stay? 🤔"
+        message = "Hit or Stay?"
     } else if (sum === 21) {
         hasBlackJack = true
-        message = "Woohoo! Blackjack! 🎉"
+        message = "Blackjack!"
     } else {
         isAlive = false
-        message = "Bust! 😭"
+        message = "Bust!"
     }
-    console.log(message)
+    messageEl.textContent = message
 }
+
+
 
