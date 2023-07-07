585cd6492a4591259c4111cb023a4527b643f59c
Brian Hartnett <brian.phartnettjr@gmail.com>
2023-07-06 22:58:44 -0400

    Finished basic game mechanics

--- index.js
+++ index.js
@@ -1,16 +1,52 @@
-let firstCard = 10
-let secondCard = 11
-let sum = firstCard + secondCard
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
+let player = {
+    name: "Brian",
+    balance: "$145.44"
+}
+
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.name
+
+let chipsEl = document.getElementById("chips-el")
+chipsEl.textContent = player.balance
+
+function getRandomCard() {
+    let randomCard = Math.floor(Math.random() * 13) + 1
+    if (randomCard === 1) {
+        return 11
+    }
+    else if (randomCard > 10) {
+        return 10
+    }
+    else {
+        return randomCard
+    }
+}
+
 function startGame() {
+    isAlive = true
+    hasBlackJack = false
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    renderGame()
+}
+
+function renderGame() {
     sumEl.textContent = "Sum: " + sum
-    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    cardsEl.textContent = "Cards: "
+        for (let i = 0; i < cards.length; i++) {
+            cardsEl.textContent += cards[i] + " "
+        }
     if (sum <= 20) {
         message = "Hit or Stay?"
     } else if (sum === 21) {
@@ -21,6 +57,16 @@ function startGame() {
         message = "Bust!"
     }
     messageEl.textContent = message
+}
+
+function newCard() {
+    if (isAlive && hasBlackJack === false) {
+        console.log("Drawing a new card")
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        renderGame()
+    }
 }
 
 
--- practice.js
+++ practice.js
@@ -17,3 +17,160 @@
 // } else {
 //     console.log("Not eligble, you've already gotten one!")
 // }
+
+// let featuredPosts = ["Check out my Netflix clone",
+// "Here's code for my projecet",
+// "I've just relaunched my portfolio"]
+
+// let experience = ["CEO at Scrimba",
+// "Frontend developer at Xeneta",
+// "People Counter for Norstat"]
+
+// console.log(experience[1])
+// console.log(experience[2])
+// console.log(experience[0])
+
+// let about = ["Bran",
+// 24,
+// true]
+
+// let cards = [7, 4]
+
+// cards.push(6)
+   
+// console.log(cards)
+
+// let messages = ["Hey, how's it going?",
+// "I'm great, thank you! How about you?",
+// "All good. Been working on my portfolio lately."]
+
+// let newMessage = "Same here!"
+
+// messages.push(newMessage)
+
+// messages.pop()
+
+// console.log(messages)
+
+// for ( let count = 10; count < 21; count += 1 ) {
+
+//     console.log(count)
+
+// }
+
+// for (let i = 10; i < 101; i += 10) {
+//     console.log(i)
+// }
+
+// let messages = ["Hey, how's it going?",
+// "I'm great, thank you! How about you?",
+// "All good. Been working on my portfolio lately.",
+// "Same here!",
+// "Nice!"]
+
+// for (let i = 0; i < messages.length; i += 1) {
+//     console.log(i)
+// }
+
+// let cards = [7, 3, 9]
+
+// for (let i = 0; i < cards.length; i++) {
+//     console.log(cards[i])
+// }
+
+// let sentence = ["Hello",
+// "my",
+// "name",
+// "is",
+// "Brian"]
+
+// let greetingEl = document.getElementById("greeting-el")
+
+// for (let i = 0; i < sentence.length; i++) {
+//     greetingEl.textContent += sentence[i] + " "
+// }
+
+// let player1Time = 102
+// let player2Time = 107
+
+// function getFastestRaceTime() {
+//     if (player1Time < player2Time) {
+//         return player1Time
+//     } else if (player1Time > player2Time) {
+//         return player2Time
+//     } else {
+//         return player1Time
+//     }
+// }
+
+// let fastestRace = getFastestRaceTime()
+
+// console.log(fastestRace)
+
+// function getTotalRaceTime() {
+//     return player1Time + player2Time
+// }
+
+// totalTime = getTotalRaceTime()
+
+// console.log(totalTime)
+
+// let randomNumber = Math.random() * 6
+
+// console.log(randomNumber)
+
+// let flooredNumber = Math.floor(3.45632)
+
+// console.log(flooredNumber)
+
+
+
+// let randomNumber = rollDice()
+// console.log(randomNumber)
+
+// function rollDice() {
+//     return Math.floor(Math.random() * 6) + 1
+// }
+
+// let hasCompletedCourse = true
+// let givesCertificate = true
+
+// function generateCertificate() {
+//     console.log("Generating certifcate...")
+// }
+
+// if (hasCompletedCourse && givesCertificate) {
+//     generateCertificate()
+// }
+
+// let hasSolvedChallenge = false
+// let hasHintsLeft = false
+
+// function showSolution() {
+//     console.log("Showing the solution...")
+// }
+
+// if (hasSolvedChallenge === false && hasHintsLeft === false) {
+//     showSolution()
+// }
+
+// let likesDocumentaries = true
+// let likesStartups = false
+
+// function recommendedMovie() {
+//     console.log("Hey, check out this new film we think you will like!")
+// }
+
+// if (likesDocumentaries || likesStartups) {
+//     recommendedMovie()
+// }
+
+let course = {
+    title: "Learn CSS Grid",
+    lessons: 16,
+    creator: "Per",
+    length: 63,
+    level: 2,
+    isFree: true,
+    tags: ["html", "css"]
+}
